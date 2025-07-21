c93143643728ba45bf666c08c693a9e64ea10611
boreljoao <boreljoao@gmail.com>
2025-07-21 18:44:46 -0300

    2.02 pronto pra upar

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // --- PARTICLES.JS ---
     particlesJS("particles-js", {
         particles: {
-            number: { value: 60, density: { enable: true, value_area: 800 } },
+            number: { value: 80, density: { enable: true, value_area: 800 } },
             color: { value: "#A855F7" },
             shape: { type: "circle" },
             opacity: { value: 0.5, random: true },
